f74461df40d9dfa0c62052ce4f5341101323ed8d
arkerphyo92 <arkerphyo92@gmail.com>
2024-06-05 17:35:09 +0700

    535setting for deployment

--- tender/conf/prod.py
+++ tender/conf/prod.py
@@ -32,6 +32,8 @@ DATABASES = {
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'tendersfiles')
+    MEDIAFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
