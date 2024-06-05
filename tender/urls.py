0c89b14e4c83745297af6856133c0ee5cf145d81
arkerphyo92 <arkerphyo92@gmail.com>
2024-06-05 17:41:30 +0700

    541setting for deployment

--- tender/urls.py
+++ tender/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-if not settings.PRODUCTION:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
