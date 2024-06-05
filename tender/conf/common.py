7a088eb5f2befb1f69275f783c4d56b69f423ad3
arkerphyo92 <arkerphyo92@gmail.com>
2024-06-05 17:29:55 +0700

    530setting for deployment

--- tender/conf/common.py
+++ tender/conf/common.py
@@ -38,7 +38,7 @@ SITE_ID = 1 #Need this because
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
